a976c5f74c5071d345a1bb5e6ac66aee5de8d11a
Manjeet Malik <Manjeet Malik@MANJEET-MALIK>
2023-05-09 06:56:12 +0530

    fixes

---
+++ RetailSurveyTool/Temp_TabbedPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RetailSurveyTool
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Temp_TabbedPage : TabbedPage
+    {
+        public Temp_TabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
